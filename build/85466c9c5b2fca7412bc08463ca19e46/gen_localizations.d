c99d6fb577096912947066f677b7f5e89b0b7b22
Amado Martin Solis <amadojms@gmail.com>
2023-10-05 19:50:44 -0400

    update code

---
+++ build/85466c9c5b2fca7412bc08463ca19e46/gen_localizations.d
@@ -0,0 +1,1 @@
+ C:\\flutter\ apps\\web2\\.dart_tool\\flutter_gen\\gen_l10n\\app_localizations_en.dart C:\\flutter\ apps\\web2\\.dart_tool\\flutter_gen\\gen_l10n\\app_localizations.dart:  C:\\flutter\ apps\\web2\\l10n.yaml C:\\flutter\ apps\\web2\\lib/src/localization\\app_en.arb
